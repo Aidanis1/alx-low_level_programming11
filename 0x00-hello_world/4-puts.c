2999b65acea3931ca3a2aa3c41eb2467b0a5f1ab
Nisrine AIDA <nisrineaida2001@gmail.com>
2023-03-16 13:09:48 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
-#!/bin/bash
 #include <stdio.h>
-	/**
-	* main - A program that prints a line with puts function
-	* Return : 0 (Succes)
-	*/
-	int main(void) {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-	}
+
+int main(void)
+{
+    printf("\"Programming is like building a multilingual puzzle\"\n");
+    return (0);
+}
