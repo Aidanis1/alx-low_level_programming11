f8790067e057be9f2c2c1d816f03310064dfb118
Nisrine AIDA <nisrineaida2001@gmail.com>
2023-03-19 13:56:18 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -print all single digit numbers of base 10
+ * return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+	
+	while (n < 10)
+	{
+		printf("%d",n);
+		n++;
+	}
+	return (0);
+}
