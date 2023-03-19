e027330b12fee99cb429101d27e4ff20776a8ce1
Nisrine AIDA <nisrineaida2001@gmail.com>
2023-03-19 14:14:54 -0700

    print number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main -print 0123456789
- * return: Always 0 (success)
+ *main - print 0123456789
+ *Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int n = 0;
-	
+
 	while (n < 10)
 	{
-		printf("%d", n);
+		printf("%i", n);
 		n++;
 	}
 	putchar('\n');
