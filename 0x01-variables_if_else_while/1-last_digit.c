ed3334b081a35dd1ff6935e4a83b62e85c88f9c0
Nisrine AIDA <nisrineaida2001@gmail.com>
2023-03-19 11:41:58 -0700

    last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("last digit of %i is %i and is ", n, (n % 10));
+
+	printf("Last digit of %i is %i and is ", n, (n % 10));
+
 	if ((n % 10) == 0)
 	{
 		printf("0\n");
@@ -26,5 +28,6 @@ int main(void)
 	{
 		printf("less than 6 and not 0\n");
 	}
+
 	return (0);
 }
