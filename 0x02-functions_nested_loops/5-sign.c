b1935c32cd368859ae192a96c9e6f3b0049524b6
Nisrine AIDA <nisrineaida2001@gmail.com>
2023-04-20 10:05:56 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,6 +5,7 @@
  * @n: number to check
  * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -12,15 +13,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-       	else if (n == 0)
+	else if (n == 0)
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 	}
-       	else if (n < 0)
+	else
 	{
 		_putchar('-');
-	}
 		return (-1);
 	}
 }
