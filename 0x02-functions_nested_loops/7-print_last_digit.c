31476855d1bb16ce4b1c20cc13f0a2776a106995
Nisrine AIDA <nisrineaida2001@gmail.com>
2023-04-20 10:41:23 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,14 @@
 
 /**
  * print_last_digit - main function
- *@n : integer to get last digit of
- *Return : last digit of n
+ *
+ * @n: integer to get last digit of
+ * Return: last digit of n
+ *
  */
 
 int print_last_digit(int n)
 {
-	-putchar('0' + _abs(n % 10));
+	_putchar('0' + _abs(n % 10));
 	return (_abs(n % 10));
 }
