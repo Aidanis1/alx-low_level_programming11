b4e5ae85a7047f39a40aaf90e7b042082f3a6530
Nisrine AIDA <nisrineaida2001@gmail.com>
2023-05-02 15:03:50 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,14 +9,15 @@
  *
  * Return: length of list
  */
-size_t listint_len(const listint_t *f)
+
+size_t listint_len(const listint_t *h)
 {
 	size_t len;
 
-	if (f == NULL)
+	if (h == NULL)
 		return (0);
 
-	for (len = 0; f != NULL; f = f->next, len++)
+	for (len = 0; h != NULL; h = h->next, len++)
 		;
 
 	return (len);
